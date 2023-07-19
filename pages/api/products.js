8e7366cc3f5a948b5aa5fffc4eb423ccccf372d5
Fernando <fernandoalcocer812@hotmail.com>
2023-07-18 23:39:02 -0300

    Finalizada 05-admin-categories y 06-admin-security

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,9 +1,11 @@
 import { Producto } from "@/models/Product";
 import { mongooseConnect } from "@/lib/mongoose";
+import { isAdminRequest } from "./auth/[...nextauth]";
 
 export default async function handle(req, res) {
   const { method } = req;
   await mongooseConnect();
+  await isAdminRequest(req, res);
 
   if (method === "GET") {
     if (req.query?.id) {
@@ -14,22 +16,25 @@ export default async function handle(req, res) {
   }
 
   if (method === "POST") {
-    const { title, description, price, images, category } = req.body;
+    const { title, description, price, images, category, properties } =
+      req.body;
     const productDoc = await Producto.create({
       title,
       description,
       price,
       images,
       category,
+      properties,
     });
     res.json(productDoc);
   }
 
   if (method === "PUT") {
-    const { title, description, price, images, category, _id } = req.body;
+    const { title, description, price, images, category, properties, _id } =
+      req.body;
     await Producto.updateOne(
       { _id },
-      { title, description, price, images, category }
+      { title, description, price, images, category, properties }
     );
     res.json(true);
   }
--- pages/components/ProductForm.js
+++ pages/components/ProductForm.js
@@ -11,10 +11,14 @@ export default function ProductForm({
   price: existingPrice,
   images: existingImages,
   category: assignedCategory,
+  properties: assignedProperties,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
   const [category, setCategory] = useState(assignedCategory || "");
+  const [productProperties, setProductProperties] = useState(
+    assignedProperties || {}
+  );
   const [price, setPrice] = useState(existingPrice || "");
   const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
@@ -31,7 +35,14 @@ export default function ProductForm({
   async function saveProduct(ev) {
     ev.preventDefault();
 
-    const data = { title, description, price, images, category };
+    const data = {
+      title,
+      description,
+      price,
+      images,
+      category,
+      properties: productProperties,
+    };
     if (_id) {
       //update
       await axios.put("/api/products", { ...data, _id });
@@ -65,6 +76,28 @@ export default function ProductForm({
   function updateImagesOrder(images) {
     setImages(images);
   }
+
+  function setProductProp(propName, value) {
+    setProductProperties((prev) => {
+      const newProductProps = { ...prev };
+      newProductProps[propName] = value;
+      return newProductProps;
+    });
+  }
+
+  const propertiesToFill = [];
+  if (categories.length > 0 && category) {
+    let catInfo = categories.find(({ _id }) => _id === category);
+    propertiesToFill.push(...catInfo.properties);
+    while (catInfo?.parent?._id) {
+      const parentCat = categories.find(
+        ({ _id }) => _id === catInfo?.parent?._id
+      );
+      propertiesToFill.push(...parentCat.properties);
+      catInfo = parentCat;
+    }
+  }
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
@@ -80,6 +113,22 @@ export default function ProductForm({
         {categories.length > 0 &&
           categories.map((c) => <option value={c._id}>{c.name}</option>)}
       </select>
+      {propertiesToFill.length > 0 &&
+        propertiesToFill.map((p) => (
+          <div className="">
+            <label>{p.name[0].toUpperCase() + p.name.substring(1)}</label>
+            <div>
+              <select
+                value={productProperties[p.name]}
+                onChange={(ev) => setProductProp(p.name, ev.target.value)}
+              >
+                {p.values.map((v) => (
+                  <option value={v}>{v}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+        ))}
       <label>Photos</label>
       <div className="mb-2 flex flex-wrap gap-1">
         <ReactSortable
@@ -89,7 +138,10 @@ export default function ProductForm({
         >
           {!!images?.length &&
             images.map((link) => (
-              <div key={link} className="h-24">
+              <div
+                key={link}
+                className="h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200"
+              >
                 <img src={link} alt="Producto" className="rounded-lg" />
               </div>
             ))}
@@ -99,7 +151,7 @@ export default function ProductForm({
             <Spinner />
           </div>
         )}
-        <label className=" w-24 h-24  text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 cursor-pointer">
+        <label className=" w-24 h-24  cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-sm bg-white shadow-ms border border-gray-200">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
